3fb139e12b62e1e18cfac15104ce414cb4afdae8
Гуля <gulya20181@yandex.ru>
2022-09-06 13:11:31 +0300

    homework002

---
+++ homework002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает
+// на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("введите число");
+int a =44;
+int b = 5;
+int c = 78;
+int max = c;
+
+if(b>a)  b = max;
+if (c>b) c= max;
+else if (c>a) c = max;
+Console.Write ("max");
+Console.WriteLine(max);
+
+
+
